3aea90e48642036b04ab10f79b9c1e053b89c5b3
andreas <andreas.ehalvorsen@gmail.com>
2026-08-13 10:20:27 +0200

    add appconfig files

---
+++ appconfig.c
@@ -0,0 +1,4 @@
+#include "appconfig.h"
+#include "termbox2.h"
+
+uintattr_t color_input_bg = TB_BLACK;
---
+++ appconfig.h
@@ -0,0 +1,15 @@
+#ifndef APPCONFIG_H
+#define APPCONFIG_H
+
+#include "termbox2.h"
+
+#define MIN_TERM_HEIGHT 10
+#define MIN_TERM_WIDTH 40
+
+#define MAX_LINE_WRAPS 7
+
+#define TIMESTAMP_COL_WIDTH 12
+
+extern uintattr_t color_input_bg;
+
+#endif
