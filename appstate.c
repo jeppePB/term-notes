dc0fb9fb41d230e5992f17a879f9c6b0eb855b31
andreas <andreas.ehalvorsen@gmail.com>
2026-08-17 11:17:30 +0200

    add tag list and count to appstate

--- appstate.c
+++ appstate.c
@@ -3,3 +3,5 @@
 FocusMode focus = FOCUS_INPUT;
 int scroll_offset = 0;
 
+char active_tags[MAX_ACTIVE_TAGS][TAG_NAME_MAX_LEN];
+int active_tag_count = 0;
--- appstate.h
+++ appstate.h
@@ -3,10 +3,17 @@
 
 typedef enum {
     FOCUS_INPUT,
-    FOCUS_NOTES
+    FOCUS_NOTES,
+    FOCUS_COMMAND
 } FocusMode;
 
 extern FocusMode focus;
 extern int scroll_offset;
+
+#define MAX_ACTIVE_TAGS 8
+#define TAG_NAME_MAX_LEN 10
+
+extern char active_tags[MAX_ACTIVE_TAGS][TAG_NAME_MAX_LEN];
+extern int active_tag_count;
 
 #endif
