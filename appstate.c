7d0c0d4cde958e470cf0f45b5b9c1fd0a03a9859
andreas <andreas.ehalvorsen@gmail.com>
2026-08-19 21:38:06 +0200

    add new running-flag. add error status message and setter.

--- appstate.c
+++ appstate.c
@@ -1,7 +1,20 @@
+#include <stdio.h>
 #include "appstate.h"
 
+// ui
 FocusMode focus = FOCUS_INPUT;
 int scroll_offset = 0;
 
+//program state
+int running = 1;
+
+//errors
+char status_message[STATUS_MSG_MAX_LEN] = {0};
+
+//tags
 char active_tags[MAX_ACTIVE_TAGS][TAG_NAME_MAX_LEN];
 int active_tag_count = 0;
+
+void status_set(const char *msg){
+    snprintf(status_message, STATUS_MSG_MAX_LEN, "%s", msg);
+}
--- appstate.h
+++ appstate.h
@@ -1,6 +1,7 @@
 #ifndef APPSTATE_H
 #define APPSTATE_H
 
+// ui
 typedef enum {
     FOCUS_INPUT,
     FOCUS_NOTES,
@@ -10,9 +11,17 @@ typedef enum {
 extern FocusMode focus;
 extern int scroll_offset;
 
+// program state
+extern int running;
+
+// errors
+#define STATUS_MSG_MAX_LEN 128
+extern char status_message[STATUS_MSG_MAX_LEN];
+void status_set(const char *msg);
+
+// tags
 #define MAX_ACTIVE_TAGS 8
 #define TAG_NAME_MAX_LEN 10
-
 extern char active_tags[MAX_ACTIVE_TAGS][TAG_NAME_MAX_LEN];
 extern int active_tag_count;
 
